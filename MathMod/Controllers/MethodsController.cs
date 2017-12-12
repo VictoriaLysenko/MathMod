72f2a5aa3ef4fc890befaa20008c202ab0721b57
Viktoria <viktorialysenko83@gmail.com>
2017-12-12 20:50:57 +0200

    Add Method Controller

---
+++ MathMod/Controllers/MethodsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MathMod.Controllers
+{
+    public class MethodsController : Controller
+    {
+        public MethodsController()
+        {
+
+        }
+        
+        public async Task<IEnumerable<double>> EulerMethod()
+        {
+            List<double> result = new List<double>();
+            return result; 
+        }
+        
+        public async Task<IEnumerable<double>> RunheKuta()
+        {
+            List<double> result = new List<double>();
+            return result;
+        }
+        
+        public async Task<IEnumerable<double>> Method3()
+        {
+            List<double> result = new List<double>();
+            return result;
+        }
+        
+        public async Task<IEnumerable<double>> Method4()
+        {
+            List<double> result = new List<double>();
+            return result;
+        }
+    }
+}
