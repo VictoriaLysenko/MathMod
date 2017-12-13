7970292e02abe539b5effaf0ce13d46d36fbbac5
qwertyinshadow <qwertyinshadow@gmail.com>
2017-12-13 22:49:57 +0300

    add params, result table

--- MathMod/Scripts/form.js
+++ MathMod/Scripts/form.js
@@ -1,26 +1,35 @@
-﻿document.forms['decideEquation'].onsubmit = function (event) {
-    let form = event.target,
-        equation = form.elements.equation.value,
-        koshi = form.elements.koshi.value,
-        method = form.elements.method.value;
+﻿$("#decideEquation").submit(function (event) {
+        event.preventDefault();
+        let form = event.target,
+            startValue = form.elements.startValue.value,
+            EndValue = form.elements.EndValue.value,
+            NumberOfStep = form.elements.NumberOfStep.value,
+            method = form.elements.method.value;
 
-    let results = document.getElementsByClassName("result")[0];
+        let results = document.getElementsByClassName("result")[0];
 
-    let params = { equation, koshi };
-    $.ajax({
-        type: "POST",
-        url: "/methods/TestPost/",
-        data: JSON.stringify(params),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: successFunc,
-        //error: errorFunc
+        let params = { startValue, EndValue, NumberOfStep };
+        $.ajax({
+            type: "POST",
+            url: `/methods/${method}/`,
+            data: JSON.stringify(params),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: successFunc,
+            //error: errorFunc
+        });
+
+        function successFunc(data, status) {
+            if (status == "success") {
+                console.log(data);
+                let resultTable = '',
+                    index = 0;
+                Object.entries(data).forEach(([key, value]) => {
+                    resultTable += `<tr><th scope="row">${index}</th><td>${key}</td><td>${value}</td></tr>`;
+                    index += 1;
+                });
+                $("#result").html(resultTable);
+               
+            }
+        }
     });
-
-    function successFunc(data, status) {
-
-    $(".result").html(data);
-        console.log(data, status, 'success');
-    }
-    return false;
-}
